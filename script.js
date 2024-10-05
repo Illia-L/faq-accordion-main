aa79d4abecf0b6af719306fd33762a2e8ad3921f
Illia Liashko <2275040@gmail.com>
2024-10-05 23:12:13 +0300

    build faq accordion

---
+++ script.js
@@ -0,0 +1,88 @@
+const section = document.querySelector('.faqs');
+
+section.addEventListener('click', e => {
+  const target = e.target;
+  console.log(target);
+  const isTargetAQuestion = target.classList.contains('faqs-question');
+  const isTargetAnOpenButton = target
+    .closest('button')
+    ?.classList.contains('faqs-button-open');
+  const isTargetAnCloseButton = target
+    .closest('button')
+    ?.classList.contains('faqs-button-close');
+
+  if (!(isTargetAQuestion || isTargetAnOpenButton || isTargetAnCloseButton))
+    return;
+
+  const questionBox = target.parentElement;
+  const paragraph = questionBox.nextElementSibling;
+  const openButton = questionBox.querySelector('.faqs-button-open');
+  const closeButton = questionBox.querySelector('.faqs-button-close');
+
+  console.log(questionBox);
+
+  toggleElement(paragraph);
+  openButton.classList.toggle('hidden');
+  closeButton.classList.toggle('hidden');
+});
+
+function collapseElement(element) {
+  // get the height of the element's inner content, regardless of its actual size
+  const sectionHeight = element.scrollHeight;
+
+  // temporarily disable all css transitions
+  const elementTransition = element.style.transition;
+  element.style.transition = '';
+
+  // on the next frame (as soon as the previous style change has taken effect),
+  // explicitly set the element's height to its current pixel height, so we
+  // aren't transitioning out of 'auto'
+  requestAnimationFrame(function () {
+    element.style.paddingTop = 0 + 'px';
+    element.style.height = sectionHeight + 'px';
+    element.style.transition = elementTransition;
+
+    // on the next frame (as soon as the previous style change has taken effect),
+    // have the element transition to height: 0
+    requestAnimationFrame(function () {
+      element.style.height = 0 + 'px';
+    });
+  });
+
+  // mark the section as "currently collapsed"
+  element.classList.add('js-collapsed');
+}
+
+function expandElement(element) {
+  console.log('expandElement runs...');
+  // get the height of the element's inner content, regardless of its actual size
+  const sectionHeight = element.scrollHeight;
+
+  console.log('sectionHeight: ', sectionHeight);
+  // have the element transition to the height of its inner content
+  element.style.paddingTop = 24 + 'px';
+  element.style.height = (sectionHeight + 24) + 'px';
+
+  // when the next css transition finishes (which should be the one we just triggered)
+  // element.addEventListener('transitionend', function(e) {
+  //   // remove this event listener so it only gets triggered once
+  //   element.removeEventListener('transitionend', arguments.callee);
+
+  //   // remove "height" from the element's inline styles, so it can return to its initial value
+  //   element.style.height = null;
+  // });
+
+  // mark the section as "currently not collapsed"
+  element.classList.remove('js-collapsed');
+}
+
+function toggleElement(element) {
+  const isCollapsed = element.classList.contains('js-collapsed');
+
+  if (isCollapsed) {
+    expandElement(element);
+    section.setAttribute('data-collapsed', 'false');
+  } else {
+    collapseElement(element);
+  }
+}
